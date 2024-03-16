b9f0627e98c4c56f726d83fbf29bc3c23b6965ab
APRENDIZ <APRENDIZ@BOGCPCDMF030306.FormacionSena.RED>
2024-03-15 19:39:12 -0500

    first commit

---
+++ Persona/Persona1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    internal class Persona1
+    {
+      
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Sexo { get; set; }
+        public string ColorPiel { get; set; }
+        public string ColorOjos { get; set; }
+        public double Estatura { get; set; }
+        public double Peso { get; set; }
+     
+
+
+        public Persona1(string nombre, DateTime fechaNacimiento, string sexo, string colorPiel, string colorOjos, double estatura, double peso)
+        {
+            Console.WriteLine("Escriba el nombre de la Persona");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Escriba la fecha de nacimiento(00-00-0000)");
+            FechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Escriba el Sexo");
+            Sexo = Console.ReadLine();
+            Console.WriteLine("Escriba el Color de Piel");
+            ColorPiel = Console.ReadLine();
+            Console.WriteLine("Escriba el Color de Ojos");
+
+            ColorOjos = Console.ReadLine();
+            Console.WriteLine("Escriba la Estatura");
+
+            Estatura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escriba el peso(0.00)");
+
+            Peso = Convert.ToDouble(Console.ReadLine());
+
+        }
+
+        public void Comer()
+        {
+            Console.WriteLine(Nombre + " está comiendo.");
+            
+        }
+
+        public void Hablar()
+        {
+            Console.WriteLine(Nombre + " está hablando.");
+        }
+
+        public void Caminar()
+        {
+            Console.WriteLine(Nombre + " está caminando.");
+        }
+
+    }
+}
+
---
+++ Persona/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    internal class Program
+    {
+        
+    static void Main(string[] args)
+        {
+            Persona1 per = new Persona1("", new DateTime(), "", "", "", 0, 0);
+
+            Console.WriteLine("Nombre: " + per.Nombre);
+            Console.WriteLine("Sexo: " + per.Sexo);
+            Console.WriteLine("Color de piel: " + per.ColorPiel);
+            Console.WriteLine("Color de ojos: " + per.ColorOjos);
+            Console.WriteLine("Estatura: " + per.Estatura + " Cm");
+            Console.WriteLine("Peso: " + per.Peso + " Kg");
+
+            bool salir = false;
+                while(!salir){
+
+                Console.WriteLine("Seleccione la Accion que desea realizar");
+                Console.WriteLine("1.Comer");
+                Console.WriteLine("2.Hablar");
+                Console.WriteLine("3.Caminar");
+                Console.WriteLine("4.Salir");
+                string opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        per.Comer();
+                        break;
+                    case "2":
+                        per.Hablar();
+                        break;
+                    case "3":
+                        per.Caminar();
+                        break;
+                    case "4":
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Digite una opcion valida");
+                        break;
+                }
+
+            }
+            
+
+
+
+
+        }
+    }
+
+
+}
+
+
